f138ca3efb4f48eb13be66eec6a635516813829d
Jorge Moreno <jmoreno@gatech.edu>
2025-10-19 22:25:16 -0700

    Start js file for solution

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Computer choice function to return “rock”, “paper” or “scissors”.
+
+console.log("Hello from javascript file")
+
+
+// Get human choice
+
+
+
+// Declare players score variables
+
+
+// Logic to play single round
+
+
+// Logic to play entire game
